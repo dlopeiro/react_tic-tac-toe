b2b291afc97de37f373537d23bfdc3788d580a57
dlopeiro <dribeir4@gmail.com>
2022-12-08 21:17:31 -0300

    gameOptions states created

--- src/components/gameOption/GameOption.jsx
+++ src/components/gameOption/GameOption.jsx
@@ -1,9 +1,15 @@
 import styles from './GameOption.module.css'
+import Icon from '../icon/Icon'
 
-function GameOption () {
+function GameOption ({ status, onClick }) {
   return (
-    <div className={styles.gameOption}>
-      
+    <div className={styles.gameOption} onClick={onClick}>
+      {
+        status === 1 && <Icon iconName="x" size="25px" />
+      }
+      {
+        status === 0 && <Icon iconName="circle" size="25px" />
+      }
     </div>
   )
 }
